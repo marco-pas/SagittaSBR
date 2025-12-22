6f851d38e5dc14cb834a429bf36fc6e3320a7bbd
Marco <143794911+marco-pas@users.noreply.github.com>
2025-12-22 11:21:14 +0100

    comments
    Updated comments for origin and direction methods to indicate they need to be updated after hits.

--- ray.h
+++ ray.h
@@ -2,7 +2,6 @@
 #define RAYH
 #include "vec3.h"
 
-
 // only device functions here
 
 class ray
@@ -10,9 +9,18 @@ class ray
     public:
         __device__ ray() {}
         __device__ ray(const vec3& a, const vec3& b) { A = a; B = b; }
-        __device__ vec3 origin() const       { return A; }
-        __device__ vec3 direction() const    { return B; }
+        __device__ vec3 origin() const       { return A; } // @@ this has to be updated after the hit
+        __device__ vec3 direction() const    { return B; } // @@ this has to be updated after every hit
         __device__ vec3 point_at_parameter(float t) const { return A + t*B; }
+        // __device__ float distance() const; 
+
+        /*
+        __device__ float distTravelled() const    { return t; }
+        __device__ float rayArea() const  { return area; }
+        __device__ int hitCount() const { return hits; }
+        __device__ float rayPolariz() const { return polariz; }
+
+        */
 
         vec3 A;
         vec3 B;
