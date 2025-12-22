4a9e1ac767aa283d96ea7a9fe1eaa458b93223e5
Marco Pasquale <marcopas@nj.pdc.kth.se>
2025-12-22 11:23:10 +0100

    comments

--- ray.h
+++ ray.h
@@ -12,6 +12,7 @@ class ray
         __device__ vec3 origin() const       { return A; } // @@ this has to be updated after the hit
         __device__ vec3 direction() const    { return B; } // @@ this has to be updated after every hit
         __device__ vec3 point_at_parameter(float t) const { return A + t*B; }
+        // __device__ float distance() const; 
 
         /*
         __device__ float distTravelled() const    { return t; }
